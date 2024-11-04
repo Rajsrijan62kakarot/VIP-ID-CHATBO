cec07a3e66344959ac1e3b002a9eff7a07272319
Rajsrijan62kakarot <ranishreya200@gmail.com>
2024-11-03 21:04:21 -0800

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ API_HASH = getenv("API_HASH", "cec0e048dd9ca00d5c20020ada584278")
 OWNER_ID = int(getenv("OWNER_ID", "7804637858"))
 STRING_SESSION = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "BRANDED_WORLD")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "BRANDED_PAID_CC")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "BRANDEDKING82")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "https://t.me/EvoKakarot")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "https://t.me/EvoKakarot")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "Not_Kakarot")
 
 # Random Start Images
 IMG = [
